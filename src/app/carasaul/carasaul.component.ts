6c2541afcd3ead6f2da67cbfeeb8dd794a5d2677
Pratik.Raj <sravani.s@smartgig.tech>
2024-05-20 20:46:08 +0530

    updated the changes

--- src/app/carasaul/carasaul.component.ts
+++ src/app/carasaul/carasaul.component.ts
@@ -9,19 +9,19 @@ export class CarasaulComponent {
 
   images = [
     {
-      src: '../../assets/toppic1.jpg',
+      src: '../../assets/top_pic1.png',
       caption: 'Give your E-waste a New Life',
       description:'Transform your e-waste into a new beginning. We are committed to revitalizing businesses by offering strategic insights, expert guidance, and tailored solutions.',
       alt: 'Description of Image 1'
     },
     {
-      src: '../../assets/toppic2.jpg',
+      src: '../../assets/top_pic12.png',
       caption: 'Unlock The Potential of  Scrap Recycling',
       description:'we are dedicated to empowering businesses to unlock their full potential through strategic insights, expert guidance, and customized solutions.',
       alt: 'Description of Image 2'
     },
     {
-      src: '../../assets/toppic1.jpg',
+      src: '../../assets/top_pic13.png',
       caption: 'Reduce, Reuse and Recycle',
       description:'Embrace the mantra of Reduce, Reuse, and Recycle. We are committed to helping businesses maximize their potential by offering strategic insights, expert guidance, and personalized solutions',
       alt: 'Description of Image 2'
@@ -61,6 +61,10 @@ export class CarasaulComponent {
   }
   goToImage(index: number) {
     this.currentImageIndex = index;
+  }
+
+  refreshPage(){
+    window.location.reload();
   }
 
 
