c87ab8c860bd971d803b9ff360693cf17ceb8000
anush <kamaanush97@gmail.com>
2024-05-20 20:59:41 +0530

    new changes Done added texts

--- src/app/carasaul/carasaul.component.ts
+++ src/app/carasaul/carasaul.component.ts
@@ -10,18 +10,18 @@ export class CarasaulComponent {
   images = [
     {
       src: '../../assets/top_pic1.png',
-      caption: 'Give your E-waste a New Life',
-      description:'Transform your e-waste into a new beginning. We are committed to revitalizing businesses by offering strategic insights, expert guidance, and tailored solutions.',
-      alt: 'Description of Image 1'
-    },
-    {
-      src: '../../assets/top_pic12.png',
       caption: 'Unlock The Potential of  Scrap Recycling',
       description:'we are dedicated to empowering businesses to unlock their full potential through strategic insights, expert guidance, and customized solutions.',
       alt: 'Description of Image 2'
     },
     {
       src: '../../assets/top_pic13.png',
+      caption: 'Give your E-waste a New Life',
+      description:'Transform your e-waste into a new beginning. We are committed to revitalizing businesses by offering strategic insights, expert guidance, and tailored solutions.',
+      alt: 'Description of Image 1'
+    },
+    {
+      src: '../../assets/top_pic12.png',
       caption: 'Reduce, Reuse and Recycle',
       description:'Embrace the mantra of Reduce, Reuse, and Recycle. We are committed to helping businesses maximize their potential by offering strategic insights, expert guidance, and personalized solutions',
       alt: 'Description of Image 2'
